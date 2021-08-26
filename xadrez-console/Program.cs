309d37ff2f0d7831cbd5461c8510df98734419c4
Silas Dias <saosilas@hotmail.com>
2021-08-26 20:05:49 -0300

    projeto inciado

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console
+{
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("digite a origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("digite o destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+
+                }
+
+
+
+                Tela.imprimirTabuleiro(partida.tab);
+
+                Console.WriteLine();
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
