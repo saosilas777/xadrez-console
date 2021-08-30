24a0ab7d99c24fb0b27261b027edaec9730cfa76
Silas Dias <saosilas@hotmail.com>
2021-08-30 19:32:33 -0300

    implementado validacao de posicoes origem e destino

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,7 +4,7 @@ using xadrez;
 
 namespace xadrez_console
 {
-   
+
 
     class Program
     {
@@ -16,29 +16,42 @@ namespace xadrez_console
 
                 while (!partida.terminada)
                 {
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.WriteLine("Turno: " + partida.turno);
+                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
-                    Console.WriteLine();
-                    Console.Write("digite a origem: ");
-                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        Console.WriteLine();
+                        Console.Write("digite a origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
 
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentoPossiveis();
 
-                    Console.Clear(); 
-                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
-                    Console.Write("digite o destino: ");
-                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentoPossiveis();
 
-                    partida.executaMovimento(origem, destino);
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.Write("digite o destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+
 
                 }
-                Tela.imprimirTabuleiro(partida.tab);
-
-                Console.WriteLine();
             }
-            catch(TabuleiroException e)
+            catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }
--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -24,6 +24,28 @@ namespace tabuleiro
         }
 
         public abstract bool[,] movimentoPossiveis();
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentoPossiveis();
+
+            for(int i = 0; i < tab.linhas; i++)
+            {
+                for(int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentoPossiveis()[pos.linha, pos.coluna];
+        }
         
 
     }
